7795350fa94bd52a37be94ff9e77873e21190b4e
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2024-06-12 16:55:50 +0700

    feat: add endpoint for update book

--- repository/book_repo.go
+++ repository/book_repo.go
@@ -12,6 +12,7 @@ type BookRepo interface {
 	GetAllBooks() ([]models.Book, error)
 	GetAllAvailableBooks() ([]models.BookAvailable, error)
 	CreateBook(b *models.Book) error
+	UpdateBook(book_id uint, b *models.BookRequest) (models.Book, error)
 }
 
 func (r *Repo) GetAllBooks() ([]models.Book, error) {
@@ -70,4 +71,52 @@ func (r *Repo) CreateBook(b *models.Book) error {
 		return helper.ErrQuery
 	}
 	return nil
+}
+
+func (r *Repo) isBookExist(book_id uint) (bool, error) {
+	book := models.Book{BookID: book_id}
+
+	//search the book
+	res := r.DB.First(&book)
+	// book exist
+	if res.Error == nil {
+		return true, nil
+	}
+
+	// error query
+	if res.Error != gorm.ErrRecordNotFound {
+		return false, helper.ErrQuery
+	}
+
+	return false, nil
+
+}
+
+func (r *Repo) UpdateBook(book_id uint, b *models.BookRequest) (models.Book, error) {
+	isExist, err := r.isBookExist(book_id)
+	if err != nil {
+		return models.Book{}, err
+	}
+
+	if !isExist {
+		return models.Book{}, helper.ErrNoData
+	}
+
+	isUnique, err := r.isBookUnique(b.BookName, b.Author)
+	if err != nil {
+		return models.Book{}, helper.ErrQuery
+	}
+
+	if !isUnique {
+		return models.Book{}, helper.ErrAuthorBookUQ
+	}
+
+	updateBook := &models.Book{BookID: book_id}
+	r.DB.First(updateBook)
+	helper.CopyNonEmptyFields(*b, updateBook)
+	res := r.DB.Save(updateBook)
+	if res.Error != nil {
+		return models.Book{}, helper.ErrQuery
+	}
+	return *updateBook, nil
 }
